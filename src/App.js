0cdeedd948344f491367f1d3dd27f454f308e17b
rodolphe37 <rodolphe.a@gmail.com>
2020-02-08 12:18:26 +0100

    Magazine Style Layout

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import React from 'react';
 // import StatBars3d from './components/StatBars3d/StatBar3d';
 // import SpiralLoading from './components/spiralLoading/SpiralLoading';
 // import LoadingPage from './components/loadingPage/LoadingPage';
-// import MagazineStyleLayout from './components/magazineStyleLayout/MagazineStyleLayout'
+import MagazineStyleLayout from './components/magazineStyleLayout/MagazineStyleLayout'
 // import LoadingPage2 from './components/loadingPage2/LoadingPage2';
 // import SolarSystemExplorer from './components/solarSystemExplorer/SolarSystemExplorer';
 // import OrderProcessSteps from './components/orderProcessSteps/OrderProcessSteps';
@@ -44,7 +44,7 @@ function App() {
   return (
     <div className="App">
       {/*<LoadingPage/>*/}
-     {/*<MagazineStyleLayout />*/}
+     <MagazineStyleLayout />
       {/*<LoadingPage2 />*/}
       {/*<SolarSystemExplorer />*/}
       {/*<OrderProcessSteps />*/}
