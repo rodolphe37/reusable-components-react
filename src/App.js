cd789def719b41b18d84fc67e63d99903defe1be
rodolphe37 <rodolphe.a@gmail.com>
2020-02-09 16:46:32 +0100

    test menu

--- src/App.js
+++ src/App.js
@@ -1,89 +1,44 @@
 import React from 'react';
-// import FlyingRocket from './components/FlyingRocket/FlyingRocket';
-// import InfiniteLoopLoader from './components/InfiniteLoopLoader/InfiniteLoopLoader';
-// import CubeWave from './components/CubeWave/CubeWave';
-// import EbookApp from './components/EbookApp/EbookApp';
-// import ToggleSwitch from './components/ToggleSwitch/ToggleSwitch';
-// import CameraAnimation3d from './components/CameraAnimation3d/CameraAnimation3d';
-// import AnimatedLantern from './components/AnimatedLantern/AnimatedLantern';
-// import FreeRadicals from './components/FreeRadicals/FreeRadicals';
-// import AnimatedRocket from './components/AnimatedRocket/AnimatedRocket';
-// import FolderPaperLogin from './components/FolderPaperLogin/FolderPaperLogin';
-// import BurgerOrPizza from './components/BurgerOrPizza/BurgerOrPizza';
-import PolaroidStyles from './components/PolaroidStyles/PolaroidStyles';
-// import PolaroidCamera from './components/polaroidCamera/PolaroidCamera';
-// import Loader31 from './components/loader31/Loader31';
-// import TooltipAnimation from './components/TooltipAnimation/TooltipAnimation';
-// import StarWarsScene from './components/starWarsScene/StarWarsScene';
-// import VwBuses from './components/VwBuses/VwBuses';
-// import CupCakeSlider from './components/CupCakeSlider/CupCakeSlider';
-// import StatBars3d from './components/StatBars3d/StatBar3d';
-// import SpiralLoading from './components/spiralLoading/SpiralLoading';
-// import LoadingPage from './components/loadingPage/LoadingPage';
-// import MagazineStyleLayout from './components/magazineStyleLayout/MagazineStyleLayout'
-// import LoadingPage2 from './components/loadingPage2/LoadingPage2';
-// import SolarSystemExplorer from './components/solarSystemExplorer/SolarSystemExplorer';
-// import OrderProcessSteps from './components/orderProcessSteps/OrderProcessSteps';
-// import Magazine3DFlip from './components/magazine3DFlip/Magazine3DFlip';
-// import FizzyButton from './components/fizzyButton/FizzyButton';
-// import ExplodingModal from './components/explodingModal/ExplodingModal';
-// import ClockWorking from './components/clockWorking/ClockWorking';
-// import Iphone6 from './components/iphone6/Iphone6';
-// import BendingEffect from './components/bendingEffect/BendingEffect';
-// import NewsCards from './components/newsCards/NewsCards';
-// import DirectionallyAwareHover from './components/directionallyAwareHover/DirectionallyAwareHover';
-// import MobileMenu from './components/mobileMenu/MobileMenu';
-// import MacBookAir3D from './components/macBookAir3d/MacBookAir3D';
-// import ParticleSystem from './components/particleSystem/ParticleSystem';
-// import CubePack from './components/cubePack/CubePack';
-// import TinyCity from './components/tinyCity/TinyCity';
-// import CheckBoxStyles from './components/checkboxStyles/CheckBoxStyles';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import FlyingRocket from './components/FlyingRocket/FlyingRocket';
+import InfiniteLoopLoader from './components/InfiniteLoopLoader/InfiniteLoopLoader';
+import CubeWave from './components/CubeWave/CubeWave';
+import Examples from './Examples';
+import './assets/css/bootstrap.min.css'
 
 
 function App() {
-  return (
-    <div className="App">
-      {/*<LoadingPage/>*/}
-     {/*<MagazineStyleLayout />*/}
-      {/*<LoadingPage2 />*/}
-      {/*<SolarSystemExplorer />*/}
-      {/*<OrderProcessSteps />*/}
-      {/*<Magazine3DFlip />*/}
-       {/*<FizzyButton />*/}
-     {/*<ExplodingModal />*/}
-      {/*<ClockWorking />*/}
-      {/*<Iphone6 />*/}
-      {/*<BendingEffect />*/}
-       {/*<NewsCards />*/}
-     {/*<DirectionallyAwareHover />*/}
-      {/*<MobileMenu />*/}
-     {/* <MacBookAir3D />*/}
-      {/*<ParticleSystem />*/}
-      {/*<CubePack />*/}
-      {/*<TinyCity />*/}
-      {/*<CheckBoxStyles />*/}
-      {/*<StarWarsScene />*/}
-     {/* <SpiralLoading />*/}
-      {/*<PolaroidCamera />*/}
-      {/*<Loader31 />*/}
-      {/*<TooltipAnimation />*/}
-      {/*<FolderPaperLogin />*/}
-       {/*<BurgerOrPizza />*/}
-     <PolaroidStyles />
-      {/*<FreeRadicals />*/}
-      {/*<AnimatedRocket />*/}
-      {/*<AnimatedLantern />*/}
-      {/*<ToggleSwitch />*/}
-      {/*<CameraAnimation3d />*/}
-      {/*<VwBuses />*/}
-      {/*<CupCakeSlider />*/}
-      {/*<StatBars3d />*/}
-      {/*<FlyingRocket />*/}
-      {/*<InfiniteLoopLoader />*/}
-      {/*<CubeWave />*/}
-      {/*<EbookApp />*/}
-    </div>
-  );
+    return (
+    <Router>
+        <div>
+          <h2>Welcome to Reusable-Components-React Examples</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/FlyingRocket'} className="nav-link">FlyingRocket</Link></li>
+            <li><Link to={'/InfiniteLoopLoader'} className="nav-link">InfiniteLoopLoader</Link></li>
+            <li><Link to={'/CubeWave'} className="nav-link">CubeWave</Link></li>
+            <li><Link to={'/InfiniteLoopLoader'} className="nav-link">InfiniteLoopLoader</Link></li>
+            <li><Link to={'/InfiniteLoopLoader'} className="nav-link">InfiniteLoopLoader</Link></li>
+            <li><Link to={'/InfiniteLoopLoader'} className="nav-link">InfiniteLoopLoader</Link></li>
+            <li><Link to={'/InfiniteLoopLoader'} className="nav-link">InfiniteLoopLoader</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Examples} />
+              <Route path='/FlyingRocket' component={FlyingRocket} />
+              <Route path='/InfiniteLoopLoader' component={InfiniteLoopLoader} />
+              <Route path='/CubeWave' component={CubeWave} />
+              <Route path='/InfiniteLoopLoader' component={InfiniteLoopLoader} />
+              <Route path='/InfiniteLoopLoader' component={InfiniteLoopLoader} />
+              <Route path='/InfiniteLoopLoader' component={InfiniteLoopLoader} />
+              <Route path='/InfiniteLoopLoader' component={InfiniteLoopLoader} />
+              <Route path='/InfiniteLoopLoader' component={InfiniteLoopLoader} />
+          </Switch>
+        </div>
+      </Router>
+    );
 }
 
 export default App;
